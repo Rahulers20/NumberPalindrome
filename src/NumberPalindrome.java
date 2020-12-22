3b059a50125e2b6948fa96816dc1fbc6294f9a3c
Rahul Ravishankar <rahulrshankar02@gmail.com>
2020-12-23 05:06:28 +0530

    Initial commit

---
+++ src/NumberPalindrome.java
@@ -0,0 +1,33 @@
+public class NumberPalindrome {
+
+
+
+
+    public static boolean isPalindrome(int number) {
+
+        int reverse = 0;
+
+        if(number < 0) {
+            number *= (-1);
+        }
+
+        int originalNumber = number;
+
+r
+        while(number > 0) {
+
+            int lastDigit = 0;
+
+            lastDigit = number % 10;
+
+            reverse = (reverse * 10) + lastDigit;
+
+            number /= 10;
+        }
+
+
+        if(reverse == originalNumber) {
+            return true;
+        } else return false;
+    }
+}
